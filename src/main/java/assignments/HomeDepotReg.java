65c48ef0c5575e45f8047b38a5c6614e133ee618
FatehKerrache <kerrachet@gmail.com>
2022-04-26 02:25:03 -0600

    start pizza hut

--- src/main/java/assignments/HomeDepotReg.java
+++ src/main/java/assignments/HomeDepotReg.java
@@ -13,35 +13,35 @@ import SePractice.BaseClasses.FKChrome;
 import SePractice.BaseClasses.FKEdge;
 
 public class HomeDepotReg {
-	static FKEdge chrome = new FKEdge("Https://homedepot.com");
+	static FKEdge browser = new FKEdge("Https://homedepot.com");
 
 	public static void main(String[] args) throws InterruptedException {
-		chrome.Open(); // WebDriver setup
-		chrome.click(By.linkText("My Account")); // click on My account link
+		browser.Open(); // WebDriver setup
+		browser.click(By.linkText("My Account")); // click on My account link
 
 		Thread.sleep(6000);
-		chrome.click(By.xpath("//span[contains(text(),'Register')]")); // go to register oage
+		browser.click(By.xpath("//span[contains(text(),'Register')]")); // go to register oage
 
 		// select personal account
-		chrome.click(By.xpath(
+		browser.click(By.xpath(
 				"//body/div[@id='single-signin']/div[@id='outer']/div[@id='single-signin__body']/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[4]/button[1]/span[1]"));
 
 		// Register form informaton
 		Thread.sleep(6000);
-		chrome.sendKey(By.xpath("//input[@id='email']"), "demotest@testse.com");
-		chrome.sendKey(By.xpath("//input[@id='password-input-field']"), "demoTest@2020");
-		chrome.sendKey(By.xpath("//input[@id='zipCode']"), "80014");
-		chrome.sendKey(By.xpath("//input[@id='phone']"), "72012345698");
+		browser.sendKey(By.xpath("//input[@id='email']"), "demotest@testse.com");
+		browser.sendKey(By.xpath("//input[@id='password-input-field']"), "demoTest@2020");
+		browser.sendKey(By.xpath("//input[@id='zipCode']"), "80014");
+		browser.sendKey(By.xpath("//input[@id='phone']"), "72012345698");
 
-		chrome.click(By.xpath(
+		browser.click(By.xpath(
 				"//body/div[@id='single-signin']/div[@id='outer']/div[@id='single-signin__body']/div[1]/div[2]/form[1]/div[5]/div[1]/div[1]/label[1]"));
 
-		chrome.getDriver().switchTo().frame(chrome.getDriver().findElement(
+		browser.getDriver().switchTo().frame(browser.getDriver().findElement(
 				By.xpath("//*[@id=\"single-signin__body\"]/div/div[2]/form/div[7]/div/div/div/div/iframe")));
 
-		chrome.getDriver().findElement(By.id("recaptcha-anchor-label")).click();
+		browser.getDriver().findElement(By.id("recaptcha-anchor-label")).click();
 
-		chrome.click(By.id("registrationCreateAnAccountButton"));
+		browser.click(By.id("registrationCreateAnAccountButton"));
 
 	}
 
